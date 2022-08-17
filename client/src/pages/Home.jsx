3a7e0f8bb95155bce985e2398737ee232d0a58f9
Ahmed Aly <aaoflaca@gmail.com>
2022-08-16 19:40:53 -0700

    commit 08162022-01

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -9,32 +9,52 @@ const Container = styled.div`
   gap: 30px;
 `;
 
-// const Loading = styled.h2`
+// const Loading = styled.div`
 //   dispaly: flex;
 //   align-items: center;
 //   justify-content: center;
 //   color: ${({ theme }) => theme.text};
+//   font-size: 20px;
+//   font-weight: bold;
 // `;
 
 const Home = ({ type }) => {
   const [videos, setVideos] = useState([]);
+  const { loading } = useSelector((state) => state.video);
 
   useEffect(() => {
     const fetchVideos = async () => {
       const res = await axiosInstance.get(`/videos/${type}`);
       setVideos(res.data);
     };
+
     fetchVideos();
+
     // eslint-disable-next-line
+    return setVideos([]);
   }, [type]);
 
   return (
-    <Container>
-      {/* {loading && <Loading> Loading ... ! </Loading>} */}
-      {videos.map((video, index) => (
-        <Card key={index} video={video} />
-      ))}
-    </Container>
+    <>
+      <h2 style={{ color: "white" }}>
+        {" "}
+        {type === "sub"
+          ? "Subscriptions"
+          : type === "history"
+          ? "History"
+          : type === "trend"
+          ? "Trending"
+          : "Home"}{" "}
+      </h2>
+      <hr style={{ margin: "1rem 0" }} />
+      {/* {<Loading> Loading ... ! </Loading>} */}
+
+      <Container>
+        {videos.map((video, index) => (
+          <Card key={index} video={video} />
+        ))}
+      </Container>
+    </>
   );
 };
 
